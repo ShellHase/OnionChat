fd0cf58ab18e6cbe4077c022f7566ceb94c3cd26
ShellHase <krieger.jacke@gmail.com>
2021-12-12 10:31:08 +0100

    Login works; Homepage styled and works; added DiscussList; Writing messages with different Profiles works; New static folder structure

--- Code/main.py
+++ Code/main.py
@@ -1,66 +1,143 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import sqlite3
+from hashlib import sha512
+import os
+import re
+
 
 app = Flask(__name__)
 
 
+# defining User class for current user
+class User:
+    def __init__(self, name, password, img):
+        self.name = name
+        self.password = password
+        self.img = img
+        self.logged_in = False
+
+# assigning new User to variable I which stands just for I
+I = User('', '', '')
+
+
+# --------------------------------------------/--------------------------------------------
 @app.route("/", methods=["GET", "POST"])
 def home():
-    user = "SpongeBob"
-    user_conn = sqlite3.connect("./static/db/user.db")  # um user zu teilnehmern hinzuzufügen (mit daten wie profilbild)
-    img = user_conn.execute(f"SELECT img FROM user WHERE user = '{user}'").fetchall()[0][0]
-    user_conn.close()
+    if request.referrer == "http://127.0.0.1:1234/login":
+        # open connection
+        admin_db = sqlite3.connect("static/db/admin.db")
 
-    pic = url_for('static', filename='sources/{}'.format(img))
+        # checking if user is already registrated or not
+        # if user registrated
+        if len(admin_db.execute(f"SELECT * FROM user WHERE name = \"{request.form.get('name')}\"").fetchall()) > 0:
+            if sha512(request.form.get("password").encode()).hexdigest() == \
+                    admin_db.execute(f"SELECT password FROM user WHERE name = \"{request.form.get('name')}\"").fetchall()[0][0]:
+                I.logged_in = True
+            else:
+                I.logged_in = False
+                return redirect("/login")
+        # if user not registrated
+        else:
+            admin_db.execute("INSERT INTO user (name, password, img) VALUES ('{}', '{}', '{}');".format(
+            request.form.get("name"),
+                sha512(request.form.get("password").encode()).hexdigest(),
+                "badBoy.jpg"))
+            I.logged_in = True
 
-    if request.method == "POST":
-        discuss_conn = sqlite3.connect("./static/db/nameDerDiskussion.db")
-        discuss_conn.execute("INSERT INTO history (user, msg) VALUES ('{}', '{}');".format(user, request.form.get('msg')))
-        discuss_conn.commit()
-        discuss_conn.close()
+        # setting up user
+        I.name = request.form.get("name")
+        I.password = sha512(request.form.get("password").encode()).hexdigest()
+        I.img = admin_db.execute(f"SELECT img FROM user WHERE name = \"{request.form.get('name')}\"").fetchall()[0][0]
 
-#    i = 0
-#    while discuss_conn.execute(f"{i} IN user spalte index"):
-#        blueprint = ""
-#        i += 1
+        # commit changes and close connection
+        admin_db.commit()
+        admin_db.close()
 
-    return render_template("index.html", pro_pic=pic)
+    # rendering template with param log_in for login state
+    return render_template("home.html", log_in=I.logged_in)
 
 
+# --------------------------------------------/login--------------------------------------------
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    pwd_state = True
+    if request.referrer == "http://127.0.0.1:1234/login":
+        pwd_state = False
+    # rendering template with param log_in for login state
+    return render_template("login.html", log_in=I.logged_in, pwd_state=pwd_state)
+
+
+# --------------------------------------------/discussion--------------------------------------------
+@app.route("/discussion", methods=["GET", "POST"])
+def discuss():
+    # connecting for managing discussions
+    db = sqlite3.connect("static/db/admin.db")
+    # if method = post
+    if request.method == "POST":
+        cmd = request.form.get("cmd")
+        # if command is + adding new discussion
+        if cmd[0] == "+":
+            count = 0
+            for i in db.execute("SELECT name FROM discussions").fetchall():
+                if i[0] == "newDiscussion" or re.search("newDiscussion_[0-9]", i[0]) is not None:
+                    count += 1
+            db.execute(f'INSERT INTO discussions VALUES '
+                       f"(\"newDiscussion{'_' + str(count) if count != 0 else ''}\")")
+
+        # removing discussion entry and .db file if exists
+        if cmd[0] == "-":
+            db.execute(f"DELETE FROM discussions WHERE name='{cmd[5:]}'")
+            if os.path.exists(f"static/db/{cmd[5:]}.db"):
+                os.remove(f"static/db/{cmd[5:]}.db")
+
+        # renaming discussion
+        if cmd == ".":
+            db.execute(f"UPDATE discussions SET name='{''}'")
+
+
+    discussions = db.execute("SELECT name FROM discussions").fetchall()
+    db.commit()
+    db.close()
+    return render_template("discussList.html", discussions=discussions)
+
+
+# --------------------------------------------/discussion/<name>--------------------------------------------
+@app.route("/discussion/<name>", methods=["GET", "POST"])
+def defaultDiscuss(name):
+    # preparing user data for using
+    pic = url_for('static', filename=f'images/{I.img}')
+
+    # checking if discussion db and of course also the tables in it exist
+    if not os.path.exists(f"./static/db/{name}.db"):
+        # open connection for displaying and writing history and declaring tables
+        discuss_conn = sqlite3.connect(f"./static/db/{name}.db")
+        discuss_conn.execute("CREATE TABLE history (name, msg)")
+        discuss_conn.execute("CREATE TABLE teilnehmer (name, img)")
+        discuss_conn.commit()
+    else:
+        # open connection for displaying and writing history
+        discuss_conn = sqlite3.connect(f"./static/db/{name}.db")
+
+    if request.method == "POST":
+        if request.referrer == f"http://127.0.0.1:1234/discussion/{name}":
+            discuss_conn.execute("INSERT INTO history (name, msg) VALUES ('{}', '{}');".format(I.name, request.form.get('msg')))
+            # if first time user writes, adding to teilnehmer
+            if len(discuss_conn.execute("SELECT * FROM teilnehmer WHERE name = '{}'".format(I.name)).fetchall()) == 0:
+                discuss_conn.execute(f"INSERT INTO teilnehmer (name, img) VALUES ('{I.name}', '{I.img}')")
+            discuss_conn.commit()
+
+    # preparing history
+    history = []
+    for name, msg in discuss_conn.execute("SELECT * FROM history").fetchall():
+        history.append((name,
+                        url_for('static', filename='images/' + discuss_conn.execute(f"SELECT img FROM teilnehmer WHERE name = '{name}'").fetchall()[0][0]),
+                        msg))
+
+    discuss_conn.close()
+
+    # rendering template with params pro_pic for profile picture, history for chat history and log_ in for login state
+    return render_template("discuss.html", pro_pic=pic, history=history, log_in=I.logged_in)
 
 
 if __name__ == "__main__":
     app.run(port=1234)  # wenn debug mode alles wird noch einmal ausgeführt
-
-
-"""
-Dinge für später:
-
-def new_user():
-    c = sqlite3.connect("./static/db/user.db")
-    c.execute("INSERT INTO user (user, img) VALUES ('{}', '{}');".format("SpongeBob", "SpongeBob.jpg"))
-    c.commit()
-    c.close()
-
-def new_teilnehmer():
-    discuss_conn = sqlite3.connect("./static/db/nameDerDiskussion.db")  # used for history und teilnehmer
-    discuss_conn.execute("INSERT INTO teilnehmer (user, img) VALUES ('{}', '{}');".format("SpongeBob", "SpongeBob.jpg"))
-    discuss_conn.commit()
-    discuss_conn.close()
-    
-def new_userTable():
-    c = sqlite3.connect("./static/db/user.db")
-    c.execute("CREATE TABLE user (user TEXT, img TEXT);")
-    c.commit()
-    c.close()
-    
-def new_discussTable():
-    discuss_conn = sqlite3.connect("./static/db/nameDerDiskussion.db")
-    discuss_conn.execute("CREATE TABLE teilnehmer (user TEXT, img TEXT)")
-    discuss_conn.execute("CREATE TABLE history (user Text, msg Text(350))")
-    discuss_conn.commit()
-    discuss_conn.close()
-"""
---
+++ Code/reset.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+c = sqlite3.connect("static/db/admin.db")
+# c.execute("CREATE TABLE user(name, password, img)")
+# c.execute("CREATE TABLE discussions(name)")
+print(c.execute("SELECT * FROM user").fetchall())  # name password img
+print(c.execute("SELECT * FROM discussions").fetchall())  # name
+c.execute("DELETE FROM user")
+# c.execute("DELETE FROM discussions")
+c.commit()
+c.close()
+
+c2 = sqlite3.connect("./static/db/defaultDiscussion.db")
+# c2.execute("CREATE TABLE history (name, msg)")
+# c2.execute("CREATE TABLE teilnehmer (name, img)")
+print(c2.execute("SELECT * FROM history").fetchall())  # name msg
+print(c2.execute("SELECT * FROM teilnehmer").fetchall())  # name img
+c2.execute("DELETE FROM history")
+c2.execute("DELETE FROM teilnehmer")
+c2.commit()
+c2.close()
